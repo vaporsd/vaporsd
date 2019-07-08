479d5e1e4c236372bcc17ba52400d62d3aa68177
Leon ROUX <Nurrl@users.noreply.github.com>
2019-07-08 18:31:01 +0200

    Switched to Node.js for the project.

---
+++ app.js
@@ -0,0 +1,11 @@
+/*  _ _ ___ ___ ___ ___ ___
+ * | | | .'| . | . |  _|_ -|   /\
+ *  \_/|__,|  _|___|_| |___|  / ,\
+ *         |_|                \__/
+ * A project that provide an API to
+ * manage dockerized web applications.
+ */
+
+let Docker = require('dockerode');
+
+let docker = new Docker();
--- src/api/v1/metrics.rs
+++
@@ -1,9 +0,0 @@
-use actix_web::{Scope, HttpRequest, HttpResponse};
-
-pub fn root<S: 'static>(_: HttpRequest<S>) -> HttpResponse {
-    HttpResponse::Ok().body("metrics")
-}
-
-pub fn main<S: 'static>(scope: Scope<S>) -> Scope<S> {
-    scope
-}
